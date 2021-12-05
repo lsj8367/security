05465d126c152efeacd57362ec5fa9ccdcc9f274
lsj8367 <lsj8367@naver.com>
2021-12-05 20:20:57 +0900

    :sparkles: default page, user 설정

---
+++ src/main/java/io/github/lsj8367/security/controller/HelloController.java
@@ -0,0 +1,14 @@
+package io.github.lsj8367.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "hello";
+    }
+
+}
