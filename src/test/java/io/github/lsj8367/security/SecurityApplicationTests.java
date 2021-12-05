07e1c26bd290bb68d3cda216688ac1b8118a85fa
lsj8367 <lsj8367@naver.com>
2021-12-05 19:35:35 +0900

    Initial commit

---
+++ src/test/java/io/github/lsj8367/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lsj8367.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
